2d2e794a54a55531f2dc8d4e0146e558a2fe04b8
xiewoc <70128845+xiewoc@users.noreply.github.com>
2025-02-22 02:12:20 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -7,61 +7,83 @@ import json
 import requests
 import io
 import base64
-import PIL as pillow
+from PIL import Image as pillow
 
-@register("astrbot_plugin_for_sd_webui", "xiewoc ", "extention in astrbot for stable diffusion webui(api)", "0.0.1", "https://github.com/xiewoc/astrbot_plugin_for_sd_webui")
-class MyPlugin(Star):
-    def __init__(self, context: Context):
+# 定义全局变量
+global url, model_name, step, sampler, height, width, CLIP, seed, prompt, negtive_prompt
+
+url = ''
+model_name = ''
+step = 0
+sampler = ''
+height = 0
+width = 0
+CLIP = 0
+seed = 0
+prompt = ''
+negtive_prompt = ''
+
+def text2image(url, model_name, step, sampler, height, width, CLIP, seed, prompt, negtive_prompt): 
+    payload = {
+        # 模型设置
+        "override_settings": {
+            "sd_model_checkpoint": model_name,
+            "sd_vae": "",
+            "CLIP_stop_at_last_layers": CLIP,
+        },
+        # 基本参数
+        "prompt": prompt,
+        "negative_prompt": negtive_prompt,
+        "steps": step,
+        "sampler_name": sampler,
+        "width": width,
+        "height": height,
+        "batch_size": 1,
+        "n_iter": 1,
+        "seed": seed,
+        "CLIP_stop_at_last_layers": CLIP,
+    }
+
+    response = requests.post(url=f'{url}/sdapi/v1/txt2img', json=payload)
+    r = response.json()
+    image = pillow.open(io.BytesIO(base64.b64decode(r['images'][0])))
+    image.save(os.path.join(os.path.dirname(os.path.abspath(__file__)),'output.png'))
+
+@register("astrbot_plugin_for_sd_webui", "xiewoc ", "extention in astrbot for stable diffusion webui(api)", "1.0.0", "https://github.com/xiewoc/astrbot_plugin_for_sd_webui")
+class astrbot_plugin_for_sd_webui(Star):
+    def __init__(self, context: Context, config: dict):
         super().__init__(context)
-    
-    # 注册指令的装饰器。
+        self.config = config
+        print(self.config)
+
+        # 获取顶级配置
+        global url
+        url = self.config.get('api_address', 'http://127.0.0.1:7860')
+
+        # 获取子配置
+        sub_config = self.config.get('sub_config', {})
+
+        global model_name, step, sampler, height, width, CLIP, seed, negtive_prompt
+        model_name = sub_config.get('model', '')
+        step = sub_config.get('step', 28)
+        sampler = sub_config.get('sampler', 'Euler a')
+        height = sub_config.get('height', 512)
+        width = sub_config.get('width', 512)
+        CLIP = sub_config.get('CLIP', 2)
+        seed = sub_config.get('seed', -1)
+        negtive_prompt = sub_config.get('negtive_prompt', 'nsfw')
+
     @filter.command("draw")
     async def draw(self, event: AstrMessageEvent, message: str):
         '''这是 /draw 指令'''
-        
-        url = "http://127.0.0.1:7860"
-        prompt = message
-        negative_prompt = "(worst quality:2), (low quality:2), (normal quality:2), lowres, ((monochrome)), ((grayscale)), bad anatomy,DeepNegative, skin spots, acnes, skin blemishes,(fat:1.2),facing away, looking away,tilted head, lowres,bad anatomy,bad hands, missing fingers,extra digit, fewer digits,bad feet,poorly drawn hands,poorly drawn face,mutation,deformed,extra fingers,extra limbs,extra arms,extra legs,malformed limbs,fused fingers,too many fingers,long neck,cross-eyed,mutated hands,polar lowres,bad body,bad proportions,gross proportions,missing arms,missing legs,extra digit, extra arms, extra leg, extra foot,teethcroppe,signature, watermark, username,blurry,cropped,jpeg artifacts,text,error,"
- 
-        payload = {
- 
-            # 模型设置
-        "override_settings":{
-              "sd_model_checkpoint": "anything-v4.0.ckpt",
-              "sd_vae": "animevae.pt",
-              "CLIP_stop_at_last_layers": 2,
-        },
- 
-            # 基本参数
-        "prompt": prompt,
-        "negative_prompt": negative_prompt,
-        "steps": 28,
-        "sampler_name": "Euler a",
-        "width": 512,
-        "height": 512,
-        "batch_size": 1,
-        "n_iter": 1,
-        "seed": -1,
-        "CLIP_stop_at_last_layers": 2,
- 
-            # 面部修复 face fix
-        "restore_faces": False,
- 
-            #高清修复 highres fix
-            # "enable_hr": True,
-            # "denoising_strength": 0.4,
-            # "hr_scale": 2,
-            # "hr_upscaler": "Latent",
-     
-        }
- 
-        response = requests.post(url=f'{url}/sdapi/v1/txt2img', json=payload)
-        r = response.json()
-        image = pillow.Image.open(io.BytesIO(base64.b64decode(r['images'][0])))
-        image.save('output.png')
+        global url, model_name, step, sampler, height, width, CLIP, seed, prompt, negtive_prompt
+        prompt = message  # 使用传入的消息作为提示词
+
+        text2image(url, model_name, step, sampler, height, width, CLIP, seed, prompt, negtive_prompt)
+
         chain = [
-            At(qq=event.get_sender_id()), # At 消息发送者
-            Plain("你要的图："), 
-            Image.fromFileSystem(r"./output.png"), # 从本地文件目录发送图片
+            At(qq=event.get_sender_id()),  # At 消息发送者
+            Plain("你要的图："),
+            Image.fromFileSystem(os.path.join(os.path.dirname(os.path.abspath(__file__)),'output.png')),  # 从本地文件目录发送图片
         ]
         yield event.chain_result(chain)
